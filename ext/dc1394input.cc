d0b50d6c4460c5d56cae4a0e0da3d0db694a1c58
Jan Wedekind <jan@wedesoft.de>
2010-10-25 14:30:42 +0100

    Created empty DC1394 class

---
+++ ext/dc1394input.cc
@@ -0,0 +1,120 @@
+/* HornetsEye - Computer Vision with Ruby
+ Copyright (C) 2006, 2007, 2008, 2009 Jan Wedekind
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#include <boost/array.hpp>
+#ifndef NDEBUG
+#include <iostream>
+#endif
+#include <malloc.h>
+#include <map>
+#include <sstream>
+#include "rubytools.hh"
+#include "dc1394input.hh"
+
+// http://v4l2spec.bytesex.org/spec-single/v4l2.html
+
+using namespace boost;
+using namespace std;
+
+VALUE DC1394Input::cRubyClass = Qnil;
+
+DC1394Input::DC1394Input(void) throw (Error)
+{
+  try {
+  } catch ( Error &e ) {
+    close();
+    throw e;
+  };
+}
+
+DC1394Input::~DC1394Input(void)
+{
+  close();
+}
+
+void DC1394Input::close(void)
+{
+}
+
+string DC1394Input::inspect(void) const
+{
+  ostringstream s;
+  s << "DC1394Input( '" << m_device << "' )";
+  return s.str();
+}
+
+bool DC1394Input::status(void) const
+{
+  return true;
+}
+
+VALUE DC1394Input::registerRubyClass( VALUE module )
+{
+  cRubyClass = rb_define_class_under( module, "DC1394Input", rb_cObject );
+  rb_define_singleton_method( cRubyClass, "new",
+                              RUBY_METHOD_FUNC( wrapNew ), 0 );
+  rb_define_method( cRubyClass, "close",
+                    RUBY_METHOD_FUNC( wrapClose ), 0 );
+  rb_define_method( cRubyClass, "read",
+                    RUBY_METHOD_FUNC( wrapRead ), 0 );
+  rb_define_method( cRubyClass, "status?",
+                    RUBY_METHOD_FUNC( wrapStatus ), 0 );
+  return cRubyClass;
+}
+
+void DC1394Input::deleteRubyObject( void *ptr )
+{
+  delete (DC1394InputPtr *)ptr;
+}
+
+VALUE DC1394Input::wrapNew( VALUE rbClass )
+{
+  VALUE retVal = Qnil;
+  try {
+    DC1394InputPtr ptr( new DC1394Input );
+    retVal = Data_Wrap_Struct( rbClass, 0, deleteRubyObject,
+                               new DC1394InputPtr( ptr ) );
+  } catch ( std::exception &e ) {
+    rb_raise( rb_eRuntimeError, "%s", e.what() );
+  };
+  return retVal;
+}
+
+VALUE DC1394Input::wrapClose( VALUE rbSelf )
+{
+  DC1394InputPtr *self; Data_Get_Struct( rbSelf, DC1394InputPtr, self );
+  (*self)->close();
+  return rbSelf;
+}
+
+VALUE DC1394Input::wrapRead( VALUE rbSelf )
+{
+  VALUE retVal = Qnil;
+  try {
+    DC1394InputPtr *self; Data_Get_Struct( rbSelf, DC1394InputPtr, self );
+    FramePtr frame( (*self)->read() );
+    retVal = frame->rubyObject();
+  } catch ( std::exception &e ) {
+    rb_raise( rb_eRuntimeError, "%s", e.what() );
+  };
+  return retVal;
+}
+
+VALUE DC1394Input::wrapStatus( VALUE rbSelf )
+{
+  DC1394InputPtr *self; Data_Get_Struct( rbSelf, DC1394InputPtr, self );
+  return (*self)->status() ? Qtrue : Qfalse;
+}
+
---
+++ ext/dc1394input.hh
@@ -0,0 +1,46 @@
+/* HornetsEye - Computer Vision with Ruby
+   Copyright (C) 2006, 2007, 2008, 2009, 2010 Jan Wedekind
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+#ifndef HORNETSEYE_DC1394INPUT_HH
+#define HORNETSEYE_DC1394INPUT_HH
+
+#include <errno.h>
+#include "error.hh"
+#include "frame.hh"
+
+class DC1394Input
+{
+public:
+  DC1394Input(void) throw (Error); // two cameras?
+  virtual ~DC1394Input(void);
+  void close(void);
+  FramePtr read(void) throw (Error);
+  bool status(void) const;
+  std::string inspect(void) const;
+  static VALUE cRubyClass;
+  static VALUE registerRubyClass( VALUE module );
+  static void deleteRubyObject( void *ptr );
+  static VALUE wrapNew( VALUE rbClass );
+  static VALUE wrapClose( VALUE rbSelf );
+  static VALUE wrapRead( VALUE rbSelf );
+  static VALUE wrapStatus( VALUE rbSelf );
+protected:
+  std::string m_device;
+};
+  
+typedef boost::shared_ptr< DC1394Input > DC1394InputPtr;
+
+#endif
+
